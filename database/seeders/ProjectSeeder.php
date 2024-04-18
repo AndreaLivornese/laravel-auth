2dbf2ae327aaaf48e5cc3b34cde6ef0edacee64a
andrealivornese <andrealivornese@gmail.com>
2024-04-18 16:03:32 +0200

    creati seeder di prova

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use Faker\Generator as Faker;
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +12,25 @@ class ProjectSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        
+        for($i=0; $i < 10 ; $i++){
+
+            $newProject = new Project();
+
+            $newProject->name=$faker->jobTitle();
+            $newProject->username_creator="xClevion";
+            $newProject->link_github = "https://github.com/AndreaLivornese";
+            $newProject->program_langs= null;
+            $newProject->img = null;
+
+            $newProject->save();
+
+        }
+
+
+
+
     }
 }
